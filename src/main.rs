fc912a57ea346d5aabcf3cce74ed5a859da4fc4a
Danielle Brook-Roberge <danielle.brook-roberge@unbounce.com>
2021-04-16 10:15:40 -0700

    Add concurrency and echoing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 use std::io::prelude::*;
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+
+fn respond(mut stream: TcpStream) {
+    let mut input = vec![];
+    stream.read_to_end(&mut input).unwrap();
+    stream.write(&input).unwrap();
+}
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:8888").expect("Failed to bind to port 8888");
 
     for connection in listener.incoming() {
         match connection {
-            Ok(mut stream) => {
-                stream.write(&[b'a']).unwrap();
+            Ok(stream) => {
+                thread::spawn(move || respond(stream));
             }
             Err(e) => eprintln!("Error establishing connection {}", e),
         }
